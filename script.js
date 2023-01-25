65664f8afb79959836e8deb64d471039a303b022
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-01-25 14:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function palindrome(str) {
   let newStr = str.replace(/\W/g, '');
   newStr = newStr.replace('_', '').toLowerCase();
+str = str.replace(/[^a-zA-Z0-9]/g, '');
   let revStr = newStr.split('').reverse().join('');
   if(newStr === revStr){
     return true;
@@ -15,6 +16,6 @@ const str = prompt('Enter a string: ');
 
 const value = palindrome(str);
 
-console.log(value);
+document.write(value);
 
 module.exports = palindrome
