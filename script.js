b3c4c93982c115998b06389bbc2e0420b86498b3
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-01-29 04:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// complete the given function
+function palindrome(str){
+		const n = str.length
+	const arr = str.toLowerCase().split("")
+	if(arr.join() == arr.reverse().join()){
+		return true
+	}
+	else{
+	  return false
+	}
 
-function palindrome(str) {
-  let newStr = str.replace(/\W/g, '');
-  newStr = newStr.replace('_', '').toLowerCase();
-newStr = str.replace(/[^a-zA-Z0-9]/g, '');
-  let revStr = newStr.split('').reverse().join('');
-  if(newStr === revStr){
-    return true;
-  }
-  return false;
 }
-
-
-const str = prompt('Enter a string: ');
-
-const value = palindrome(str);
-
-document.write(value);
-
 module.exports = palindrome
